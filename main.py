07bffd9be4a29f5b6a5f720cbecabb16db0196ba
password <moto198720@yandex.ru>
2022-06-15 14:12:23 +0300

    Task 2

--- main.py
+++ main.py
@@ -1,4 +1,47 @@
-# Задача 1
+# # Задача 1
+#
+# class Student:
+#     def __init__(self, name, surname, gender):
+#         self.name = name
+#         self.surname = surname
+#         self.gender = gender
+#         self.finished_courses = []
+#         self.courses_in_progress = []
+#         self.grades = {}
+#
+#
+# class Mentor:
+#     def __init__(self, name, surname):
+#         self.name = name
+#         self.surname = surname
+#         self.courses_attached = []
+#
+#     def rate_hw(self, student, course, grade):
+#         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+#             if course in student.grades:
+#                 student.grades[course] += [grade]
+#             else:
+#                 student.grades[course] = [grade]
+#         else:
+#             return 'Ошибка'
+# class Lecturer(Mentor):
+#     pass
+# class Reviewer(Mentor):
+#     pass
+#
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.courses_in_progress += ['Python']
+#
+# cool_mentor = Mentor('Some', 'Buddy')
+# cool_mentor.courses_attached += ['Python']
+#
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+#
+# print(best_student.grades)
+
+# Задача 2
 
 class Student:
     def __init__(self, name, surname, gender):
@@ -9,6 +52,16 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecture(self, lecturer, course, grade):
+        if isinstance(lecturer,
+                      Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -16,6 +69,12 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+    grades = {}
+
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -24,19 +83,24 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-class Lecturer(Mentor):
-    pass
-class Reviewer(Mentor):
-    pass
+
+
+best_lecturer = Lecturer('Some', 'Man')
+best_lecturer.courses_attached += ['Python']
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+best_student.rate_lecture(best_lecturer, 'Python', 10)
+best_student.rate_lecture(best_lecturer, 'Python', 9)
+best_student.rate_lecture(best_lecturer, 'Python', 9)
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
 
-print(best_student.grades)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+
+print(f"Оценки лучшего студента: {best_student.grades}")
+print(f"Оценки лучшего лектора, выставленные лучшим студентом: {best_lecturer.grades}")
