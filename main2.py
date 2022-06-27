5d2f3cf4c75644077a0ce0d1b8dd9f7dda252ad4
password <moto198720@yandex.ru>
2022-06-27 13:03:24 +0300

    Task 1 and 2

--- main2.py
+++ main2.py
@@ -1,18 +1,41 @@
+
 file_name = "recipes.txt"
 
-def catalog_reader(file_name):
-    with open(file_name) as file_obj:
-        result = {}
-        for line in file_obj:
-            product_name = line
-            print(f"ingredient_name: {product_name}")
-            goods = []
-            for item in range(int(file_obj.readline())):
-                good = file_obj.readline()
-                goods.append(good.strip())
-            result[product_name] = goods
-            file_obj.readline()
-        return result
+# Задача 1:
 
+from pprint import pprint
+#
+def cook_book(file_name):
+    with open(file_name, 'r', encoding='utf-8') as file:
+        cook_book = {}
+        for line in file:
+            dish_name = line.strip()
+            cook_book[dish_name] = []
+            for i in range(int(file.readline())):
+                composition = file.readline().split(' | ')
+                cook_book[dish_name].append({'ingredient_name': composition[0],
+                                             'quantity': int(composition[1]),
+                                             'measure': composition[2].strip()})
+            file.readline()
+    return cook_book
 
-catalog_reader(file_name)
+pprint(cook_book(file_name))
+
+# Задача 2
+
+def get_shop_list_by_dishes(dishes, cook_book, person_count):
+    result = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for composition in cook_book[dish]:
+                if composition['ingredient_name'] in result:
+                    result[composition['ingredient_name']]['quantity'] += composition['quantity'] * person_count
+                else:
+                    result[composition['ingredient_name']] = {'measure': composition['measure'],
+                                                              'quantity': (composition['quantity'] * person_count)}
+        else:
+            print(f'Рецепт для "{dish}" отсутствует в кулинарной книге')
+    return result
+
+pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], cook_book('recipes.txt'), 2))
+
